3c5a0aa8c02dd04b05eb6322528ccd87bb7f01ae
Snow Arctic <119104736+keanesnow98@users.noreply.github.com>
2023-01-06 04:28:25 +0200

    Finished to How To Play

--- Assets/Scripts/Dialogs/Highscores.cs
+++ Assets/Scripts/Dialogs/Highscores.cs
@@ -1,21 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-namespace MathFighter.Core
+namespace MathFighter.Dialogs
 {
-    public class GameManager : MonoBehaviour
+    public class Highscores : MonoBehaviour
     {
         // Start is called before the first frame update
         void Start()
         {
-
         }
 
         // Update is called once per frame
         void Update()
         {
-
         }
     }
 }
--- Assets/Scripts/Events/AnimationEvents.cs
+++ Assets/Scripts/Events/AnimationEvents.cs
@@ -1,6 +1,6 @@
-using MathFighter.Scenes;
 using System.Collections;
 using System.Collections.Generic;
+using MathFighter.Scenes;
 using UnityEngine;
 
 namespace MathFighter.Events
@@ -8,25 +8,21 @@ namespace MathFighter.Events
     public class AnimationEvents : MonoBehaviour
     {
         [SerializeField]
-        public GameObject m_MainMenu;
-        [SerializeField]
         public MainScene m_MainScene;
+
         // Start is called before the first frame update
         void Start()
         {
-
         }
 
         // Update is called once per frame
         void Update()
         {
-
         }
 
         public void OnTitleImageFinished()
         {
-            m_MainMenu.SetActive(true);
-            m_MainMenu.GetComponent<Animator>().SetTrigger("Enter");
+            m_MainScene.OnTitleImageExited();
         }
 
         public void ReleasePending()
